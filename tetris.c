de6321bad4d3c68a70fff6c76921167650a45369
dardematheus <matheusdarde9@gmail.com>
2025-09-03 15:02:49 -0300

    bitmask

--- tetris.c
+++ tetris.c
@@ -10,7 +10,28 @@ typedef enum Tetromino{
     I, O, T, S, Z, J, L, COUNT
 }Tetromino;
 
+//uint16_t matrice used to hold the bitmasks
+//TYPE is Y AXIS, ROTATION is X AXIS
+static const uint16_t type[COUNT][4] = {
+    //Square O
+    { 0xCC00, 0xCC00, 0xCC00, 0xCC00 },
+    //T invertido sla
+    { 0x4E00, 0x8C80, 0xE400, 0x4C40 },
+    //I
+    { 0xF000, 0x8888, 0xF000, 0x8888 },
+    //L
+    { 0x8E00, 0xC880, 0xE200, 0x44C0 },
+    //Outro L
+    { 0x2E00, 0x88C0, 0xE800, 0xC440 },
+    //Z
+    { 0x6C00, 0x8C40, 0x6C00, 0x8C40 },
+    //Outro Z
+    { 0xC600, 0x4C80, 0xC600, 0x4C80 },
+}; 
+
 //RUNNING: 0xFF || LOCK: 0xF0 || LOST: 0x00
+//RUNNING: JOGADOR CONTROLANDO PECA
+//LOCKED: PECA TRANCADA, PRECISA GERAR UMA NOVA
 typedef struct Game {
     unsigned int points;
     uint8_t state;
@@ -18,20 +39,19 @@ typedef struct Game {
 }Game;
 
 typedef struct Piece {
-    int coordX, coordY;
-    uint8_t rot;
-    uint8_t type;
+    int coordX, coordY, rot, type;
 }Piece;
 
-Game initGame(uint8_t board[X_AXIS][Y_AXIS]);
-void drawGame(Game game, uint8_t board[X_AXIS][Y_AXIS]);
+Game initGame(void);
+void drawBoard(Game game);
+void genPiece(Game game);
 
-static uint8_t board[X_AXIS][Y_AXIS]; // 0 eh vazio, >0 eh preenchido com id da peca
+static uint8_t board[X_AXIS][Y_AXIS]; // 0 eh vazio, 1 nao
 
 int main(void)
 {
     char key;
-    Game game = initGame(board);
+    Game game = initGame();
 
     initscr();
     noecho();
@@ -39,16 +59,20 @@ int main(void)
     keypad(stdscr, TRUE);
     
     while(key != 'q'){
-        drawGame(game, board);
+        drawBoard(game);
         refresh();
         key = getch();
+        if(game.state == 0xF0){
+            genPiece(game);
+            game.state = 0xFF;
+        }
     }
 
     endwin();
     return 0;
 }
 
-Game initGame(uint8_t board[X_AXIS][Y_AXIS])
+Game initGame(void)
 {
     int x, y;
     
@@ -61,15 +85,15 @@ Game initGame(uint8_t board[X_AXIS][Y_AXIS])
     }
     Game game;
     game.points = 0;
-    game.state = 0xFF;
+    game.state = 0x00;
     game.gravity = 0;
     return game;
 }
 
-void drawGame(Game game, uint8_t board[X_AXIS][Y_AXIS])
+void drawBoard(Game game)
 {
     if(game.state == 0x00){
-        mvprintw(0, 0, "You Lost, press R to try again");
+        mvprintw(0,0, "You lost, press R to try again");
         return;
     }
 
@@ -79,6 +103,20 @@ void drawGame(Game game, uint8_t board[X_AXIS][Y_AXIS])
             else if(board[x][y] == 0) mvaddch(y, x, ' ');
         }
     }
-
     mvprintw(0, X_AXIS + 1, "Points: %d", game.points);
+}
+
+void genPiece(Game game)
+{
+    if(game.state == 0xF0 || 0x00){
+        return;
+    }
+
+    Piece piece;
+    piece.coordX = 5;
+    piece.coordY = 2;
+    piece.type = rand() % COUNT;
+    piece.rot = rand() % 4;
+
+    
 }
